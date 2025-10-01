88de05a7027bff125615bbee6f8612589991b146
m1m0zzz <contact.m1m0zzz@gmail.com>
2025-10-01 20:07:09 +0900

    fix

--- src/app/wav-gen/page.tsx
+++ src/app/wav-gen/page.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next"
+import { Suspense } from "react"
 import { Main } from "./Main"
 
 export const metadata: Metadata = {
@@ -9,7 +10,9 @@ export default function WavGen() {
   return (
     <div className="min-h-screen prose prose-stone prose-xl xl:prose-2xl dark:prose-invert p-4 sm:p-6 mx-auto">
       <div className="text-foreground text-3xl lg:text-4xl xl:text-5xl mb-6">WavGen</div>
-      <Main />
+      <Suspense>
+        <Main />
+      </Suspense>
     </div>
   )
 }
