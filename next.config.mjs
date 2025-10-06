3da2be34cb9e943e9d97c0a27d78c92472826943
m1m0zzz <contact.m1m0zzz@gmail.com>
2025-10-07 02:42:11 +0900

    add: sentry

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 import createBundleAnalyzer from "@next/bundle-analyzer"
 import createMDX from "@next/mdx"
+import { withSentryConfig } from "@sentry/nextjs"
 import rehypePrettyCode from "rehype-pretty-code"
 import remarkGfm from "remark-gfm"
 
@@ -40,4 +41,35 @@ const withMDX = createMDX({
   },
 })
 
-export default withBundleAnalyzer(withMDX(nextConfig))
+export default withSentryConfig(withBundleAnalyzer(withMDX(nextConfig)), {
+  // For all available options, see:
+  // https://www.npmjs.com/package/@sentry/webpack-plugin#options
+
+  org: "mimoz",
+
+  project: "javascript-nextjs",
+
+  // Only print logs for uploading source maps in CI
+  silent: !process.env.CI,
+
+  // For all available options, see:
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  widenClientFileUpload: true,
+
+  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+  // This can increase your server load as well as your hosting bill.
+  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+  // side errors will fail.
+  tunnelRoute: "/monitoring",
+
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  disableLogger: true,
+
+  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+  // See the following for more information:
+  // https://docs.sentry.io/product/crons/
+  // https://vercel.com/docs/cron-jobs
+  automaticVercelMonitors: true,
+})
